8e0901159b66750caa76cffbf22c0de1f399fab9
Vaughn Kottler <vaughn@libre-embedded.com>
2025-12-03 03:56:22 -0600

    0.9.3 - Add more theme colors

--- svgen/__init__.py
+++ svgen/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.2.3
-# hash=8965117fa454f319ec9304fee33bce06
+# hash=17fb0f06faf3c809dd419e0473ffe7f5
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "A tool for working with scalable vector graphics."
 PKG_NAME = "svgen"
-VERSION = "0.9.2"
+VERSION = "0.9.3"
